213025e5d6c4ee87e61c210ff5a5a47a4b299594
Aakash Mishra <aakashmishra.281@gmail.com>
2023-10-13 07:37:57 +0530

    customized Error obj

--- app.js
+++ app.js
@@ -23,7 +23,16 @@ const graphqlResolver = require('./graphql/resolver');
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver, 
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+        if(!err.originalError){
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occured';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data }
+    }
 }));
 
 //only runs incase of no response from controllers - 
